538664d1bed9ed9f66667818fc5000de1109ab75
Ezequiel Bibé <ezequielbibee@hotmail.com>
2022-04-26 20:10:17 -0300

    Desafio 2 1.0

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ function verificarUsuario() {
 
     for(i=1; i <= 5; i++) {
         nombre = prompt("Ingrese su nombre de usuario.");
-        password = prompt("Ingrese su contrase;a");
+        password = prompt("Ingrese su contraseña");
         cuenta = 5 - i;
         if(nombre != "CoderHouse" && password != "coder123") {
             if(cuenta != 0) {
-                alert(`Usuario o contrase;a erronea, por favor vuelva a intentarlo. Le quedan ${cuenta} intentos.`);
+                alert(`Usuario o contraseña erronea, por favor vuelva a intentarlo. Le quedan ${cuenta} intentos.`);
             } else {
                 alert(`Te quedaste sin intentos, vuelva a intentarlo mas tarde.`);
                 break
